c1cdcc0d0248c54f680edc2c94939acff3f81954
Juan Martinez <arribajuan@gmail.com>
2022-03-06 08:30:39 -0800

    Day 6 - implementation and additional unit tests

--- day06/modules/username_generator.py
+++ day06/modules/username_generator.py
@@ -1,3 +1,6 @@
+import re
+
+
 class UsernameGenerator:
     def __init__(self):
         pass
@@ -12,4 +15,19 @@ class UsernameGenerator:
         :param email: email to derive the username from
         :return: the generated username
         """
-        return ""
+        assert (self.is_email(email)), "Email address is not valid ({email_address})".format(email_address=email)
+        return email[0:email.find("@")]
+
+    def is_email(self, email: str) -> bool:
+        """
+        Tests the supplied email address for validity
+
+        :rtype: bool
+        :param email: The email address to be validated
+        :return: True if the email address is valid, False otherwise
+        """
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if (re.fullmatch(regex, email)):
+            return True
+        else:
+            return False
--- day06/tests/username_generator_test.py
+++ day06/tests/username_generator_test.py
@@ -1,10 +1,45 @@
 from day06.modules import username_generator
+import pytest
+
 
 class TestUsernameGenerator:
     def test_1(self):
-        email = "ben@gmail.com"
+        email = "john@gmail.com"
 
         ug = username_generator.UsernameGenerator()
         result = ug.user_name(email)
 
-        assert result == "ben"
+        assert result == "john"
+
+    def test_2(self):
+        email = "john.doe@gmail.com"
+
+        ug = username_generator.UsernameGenerator()
+        result = ug.user_name(email)
+
+        assert result == "john.doe"
+
+    def test_3(self):
+        email = "john.doe@gmail_com"
+
+        with pytest.raises(AssertionError) as e_info:
+            ug = username_generator.UsernameGenerator()
+            result = ug.user_name(email)
+
+        assert str(e_info.value) == "Email address is not valid (john.doe@gmail_com)"
+
+    def test_4(self):
+        email = "john.doe@gmail.com"
+
+        ug = username_generator.UsernameGenerator()
+        result = ug.is_email(email)
+
+        assert result == True
+
+    def test_5(self):
+        email = "john.doe@gmail_com"
+
+        ug = username_generator.UsernameGenerator()
+        result = ug.is_email(email)
+
+        assert result == False
