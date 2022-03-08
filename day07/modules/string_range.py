2a22beb391545bdb4ba8bb0f085c1f914e0c9f24
Juan Martinez <jmartinez@benchsci.com>
2022-03-07 21:48:28 -0500

    Day7 - implementation

--- day07/modules/string_range.py
+++ day07/modules/string_range.py
@@ -9,28 +9,16 @@ class StringRange:
         For example, if you pass 6 as an argument, your function should return ‘0.1.2.3.4.5’.
 
         :rtype: str
-        :param range: The number of integers to add to the string
-        :return: The string with the nunmbers separated by dots
+        :param range_count: The number of integers to add to the string
+        :return: The string with the numbers separated by dots
         """
         result = ""
 
+        if range_count > 0:
+            result = str(0)
 
-        for n in range(range_count):
-            print(n)
-
+        if range_count > 1:
+            for n in range(1, range_count):
+                result = f"{result}.{str(n)}"
 
         return result
-
-    def is_email(self, email: str) -> bool:
-        """
-        Tests the supplied email address for validity
-
-        :rtype: bool
-        :param email: The email address to be validated
-        :return: True if the email address is valid, False otherwise
-        """
-        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-        if (re.fullmatch(regex, email)):
-            return True
-        else:
-            return False
