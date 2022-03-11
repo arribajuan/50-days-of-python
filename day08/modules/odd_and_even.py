a8059ac9c99a25cb79dce317fc487333738e95c0
Juan Martinez <jmartinez@benchsci.com>
2022-03-11 16:46:01 -0500

    Day8 - implementation

--- day08/modules/odd_and_even.py
+++ day08/modules/odd_and_even.py
@@ -12,7 +12,19 @@ class OddAndEven:
         :param numbers: List of numbers to find even and odd numbers
         :return: Difference between the largest even number and the smallest odd number
         """
-        result = 0
+        numbers.sort()
 
-        return result
+        largest_even_number = 0
+        for number in reversed(numbers):
+            if number % 2 == 0:
+                largest_even_number = number
+                break
+
+        smallest_odd_number = 0
+        for number in numbers:
+            if number % 2 != 0:
+                smallest_odd_number = number
+                break
+
+        return largest_even_number - smallest_odd_number
 
