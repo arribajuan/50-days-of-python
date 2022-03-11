50f5f10f4a479fb5b189a7a35080fef80b57ff2f
Juan Martinez <jmartinez@benchsci.com>
2022-03-11 16:33:41 -0500

    Day8 - stub and unit tests

---
+++ day08/modules/odd_and_even.py
@@ -0,0 +1,18 @@
+class OddAndEven:
+    def __init__(self):
+        pass
+
+    def odd_and_even(self, numbers: list) -> int:
+        """
+        Returns the difference between the largest even number in the list and the smallest odd number in the list.
+
+        For example, if you pass [1,2,4,6] as an argument the function should return 6 -1= 5.
+
+        :rtype: int
+        :param numbers: List of numbers to find even and odd numbers
+        :return: Difference between the largest even number and the smallest odd number
+        """
+        result = 0
+
+        return result
+
---
+++ day08/tests/odd_and_even_test.py
@@ -0,0 +1,35 @@
+from day08.modules import odd_and_even
+
+
+class TestOddAndEven:
+    def test_1(self):
+        number_list = []
+
+        oae = odd_and_even.OddAndEven()
+        result = oae.odd_and_even(number_list)
+
+        assert result == 0
+
+    def test_2(self):
+        number_list = [1,3,5]
+
+        oae = odd_and_even.OddAndEven()
+        result = oae.odd_and_even(number_list)
+
+        assert result == -1
+
+    def test_3(self):
+        number_list = [2,4,6]
+
+        oae = odd_and_even.OddAndEven()
+        result = oae.odd_and_even(number_list)
+
+        assert result == 6
+
+    def test_4(self):
+        number_list = [1, 2, 4, 6]
+
+        oae = odd_and_even.OddAndEven()
+        result = oae.odd_and_even(number_list)
+
+        assert result == 5
